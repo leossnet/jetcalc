40018a7124719c21b2d10c14daf53f00c05ace0b
Koviazin Dmitry <kov-av@yandex.ru>
2017-06-27 17:44:14 +0300

    install

--- modules/modules/api.js
+++ modules/modules/api.js
@@ -559,12 +559,12 @@ router.put('/settings', HP.TaskAccess("IsModulesAdmin"), function (req, res, nex
         })
     })    
 })
-
+/*
 Compiller.Build(function(err){
     console.log("Build")    
 });
 
-/*
+
 
 setTimeout(function(){
   var path = __base+"modules";
