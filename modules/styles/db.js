adfba93606b42e42ac5386215d70d582a58df3b3
Koviazin Dmitry <kov-av@yandex.ru>
2017-08-01 10:08:57 +0300

    translate

---
+++ modules/styles/db.js
@@ -0,0 +1,16 @@
+var  crypto   = require('crypto');
+
+module.exports = {
+	models:{
+		style:{
+			CSS        : {template:"pre_textarea"},
+			IsReport   : {type : Boolean, default : false},
+			IsForm	   : {type : Boolean, default : false}
+		}
+	},
+	schema: {
+		
+
+	}
+}
+
