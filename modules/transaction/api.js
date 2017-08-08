cc4776d865bb5c4c9ee6fb48868adddabe95583a
Koviazin Dmitry <kov-av@yandex.ru>
2017-08-08 12:33:00 +0300

    JC-264 Добавить в системные настройки раздел "Транзакции", в котором разместить все справочники, связанные с настройкой бизтранов

---
+++ modules/transaction/api.js
@@ -0,0 +1,10 @@
+var mongoose   = require('mongoose');
+var router     = require('express').Router();
+var _          = require('lodash');
+var async      = require('async');
+var moment      = require('moment');
+var LIB = require(__base+'lib/helpers/lib.js');
+var HP = LIB.Permits; 
+
+  
+module.exports = router;
