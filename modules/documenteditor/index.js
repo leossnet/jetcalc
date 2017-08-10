40acc304e0b16858503bb5277c7734e5aa37665e
anssar <rumyancevandr@yandex.ru>
2017-08-10 19:01:07 +0500

    merge

--- modules/documenteditor/index.js
+++ modules/documenteditor/index.js
@@ -1,100 +1,117 @@
-var MDocumentEditor = (new function() {
-	
-	var self = new Module("documenteditor");
+var MDocumentEditor = (new function () {
 
-	self.Document = ko.observable();
+    var self = new Module("documenteditor");
 
-	self.IsAvailable = function(CodeDoc){
-		return PermChecker.CheckPrivelege("IsDocumentDesigner",CxCtrl.CxPermDoc());
-	}
- 
-	self.Init = function (done){
-		MSandBox.Events.on('sandbox_status_change',function(){
-			self.Load();
-		})
-		return done && done();
-	}
+    self.Document = ko.observable();
 
-	self.ContextChange = function(){
+    self.IsAvailable = function (CodeDoc) {
+        return PermChecker.CheckPrivelege("IsDocumentDesigner", CxCtrl.CxPermDoc());
+    }
 
-	}
+    self.Init = function (done) {
+        MSandBox.Events.on('sandbox_status_change', function () {
+            self.Load();
+        })
+        return done && done();
+    }
 
-	self.BeforeShow = function(){
-		self.Load();
-		MSite.Events.off("save",self.SaveChanges);
-        MSite.Events.on("save",self.SaveChanges);		
-	}
+    self.ContextChange = function () {
 
-	self.Mode.subscribe(function(){
-		self.Load();
-	})
+    }
 
-	self.BeforeHide = function(){
-		self.Document(null);
-		MSite.Events.off("save",self.SaveChanges);
-	}
+    self.BeforeShow = function () {
+        self.Load();
+        MSite.Events.off("save", self.SaveChanges);
+        MSite.Events.on("save", self.SaveChanges);
+    }
 
+    self.Mode.subscribe(function () {
+        self.Load();
+    })
 
-	self.DocFields = ['CodeDoc','NameDoc','SNameDoc','PrintNameDoc','PrintNumDoc','IsPrimary','IsAnalytic','IsOlap','IsInput','IsChart','IsPresent','IsShowMeasure','CodeMeasure','CodeGrp','FirstYear'];
+    self.BeforeHide = function () {
+        self.Document(null);
+        MSite.Events.off("save", self.SaveChanges);
+    }
 
 
-	self.InfoByMode = function(){
-		var Result = {Fields:self.DocFields, Links :[]};
-		switch(self.Mode()){
-			case "RootRows":
-				Result.Links = Result.Links.concat(['docrow','docheader']);
-				Result.Fields = Result.Fields.concat(['IsShowRoots']);
-			break;
-			case "Placement":
-				Result.Links = Result.Links.concat(['docfolderdoc','docpacket']);
-				Result.Fields = Result.Fields.concat(['CodeModel','IndexDoc']);
-			break;
-			case "Responsables":
-				Result.Links = Result.Links.concat(['doclabel']);
-				Result.Fields = Result.Fields.concat(['CodeRole','FirstYear','IsDesigner','IsTester']);
-			break;
-			case "ChildObjs":
-				Result.Links = Result.Links.concat(['docobjtype']);
-				Result.Fields = Result.Fields.concat(['HasChildObjs','IsDivObj','IsObjToRow','IsShowParentObj',"IsObjTree"]);
-			break;
-			case "ReportSettings":
-				Result.Links = Result.Links.concat(['docparamkey']);
-				Result.Fields = Result.Fields.concat(['IsActiveCondition']);
-			break;
-			case "Bistran":
-				Result.Links = Result.Links.concat(['docbill']);
-				Result.Fields = Result.Fields.concat(['IsBiztranDoc','UseProd','UseOrg','UseDogovor','UseDogovorArt']);
-			break;
-		}
-		return Result;
-	}
+    self.DocFields = ['CodeDoc', 'NameDoc', 'SNameDoc', 'PrintNameDoc', 'PrintNumDoc', 'IsPrimary', 'IsAnalytic', 'IsOlap', 'IsInput', 'IsChart', 'IsPresent', 'IsShowMeasure', 'CodeMeasure', 'CodeGrp', 'FirstYear'];
 
 
-	self.Load = function(){
-		if (!self.Mode()) return self.InitSetMode("RootRows");
-		self.rGet("document/"+CxCtrl.CodeDoc(),self.InfoByMode(),function(data){
-			self.Document(ModelEdit.Model("doc",data.Doc));
-		})
-	}
+    self.InfoByMode = function () {
+        var Result = {
+            Fields: self.DocFields,
+            Links: []
+        };
+        switch (self.Mode()) {
+            case "RootRows":
+                Result.Links = Result.Links.concat(['docrow', 'docheader']);
+                Result.Fields = Result.Fields.concat(['IsShowRoots']);
+                break;
+            case "Placement":
+                Result.Links = Result.Links.concat(['docfolderdoc', 'docpacket']);
+                Result.Fields = Result.Fields.concat(['CodeModel', 'IndexDoc']);
+                break;
+            case "Responsables":
+                Result.Links = Result.Links.concat(['doclabel']);
+                Result.Fields = Result.Fields.concat(['CodeRole', 'FirstYear', 'IsDesigner', 'IsTester']);
+                break;
+            case "ChildObjs":
+                Result.Links = Result.Links.concat(['docobjtype']);
+                Result.Fields = Result.Fields.concat(['HasChildObjs', 'IsDivObj', 'IsObjToRow', 'IsShowParentObj', "IsObjTree"]);
+                break;
+            case "ReportSettings":
+                Result.Links = Result.Links.concat(['docparamkey']);
+                Result.Fields = Result.Fields.concat(['IsActiveCondition']);
+                break;
+            case "Bistran":
+                Result.Links = Result.Links.concat(['docbill']);
+                Result.Fields = Result.Fields.concat(['IsBiztranDoc', 'UseProd', 'UseOrg', 'UseDogovor', 'UseDogovorArt']);
+                break;
+        }
+        return Result;
+    }
 
-	self.SaveChanges = function(){
-		self.rPut("document/"+CxCtrl.CodeDoc(),{
-			Info :self.InfoByMode(),
-			Changes:JSON.stringify(self.Document().toJS())
-		},function(){
-			swal("","Изменения сохранены","success")				
-			self.Load();
-		})
-	}
+
+    self.Load = function () {
+        if (!self.Mode()) return self.InitSetMode("RootRows");
+        self.rGet("document/" + CxCtrl.CodeDoc(), self.InfoByMode(), function (data) {
+            self.Document(ModelEdit.Model("doc", data.Doc));
+        })
+    }
+
+    self.SaveChanges = function () {
+        self.rPut("document/" + CxCtrl.CodeDoc(), {
+            Info: self.InfoByMode(),
+            Changes: JSON.stringify(self.Document().toJS())
+        }, function () {
+            swal("", "Изменения сохранены", "success")
+            self.Load();
+        })
+    }
 
 
-	self.RollBack = function(){
-		self.Load();		
-	}
+    self.RollBack = function () {
+        self.Load();
+    }
 
 
-	return self;
+    return self;
 })
 
 ModuleManager.Modules.DocumentEditor = MDocumentEditor;
 
+var ExtractObject = function (obj) {
+    var ret = obj.toJS();
+    _.keys(obj).forEach(function (k) {
+        console.log(k)
+        if (k.startsWith("Code") && k != "Code") {
+
+        }
+        if (k.startsWith("Link_")) {
+            obj[k]().forEach(function (lk) {})
+        }
+        
+    })
+    return JSON.stringify(ret, null, '\t');
+}
