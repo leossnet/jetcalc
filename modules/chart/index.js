17d21d9db6641a9e8304bdd68137c51bf11de6a2
anssar <rumyancevandr@yandex.ru>
2017-08-15 15:50:29 +0500

    fix

--- modules/chart/index.js
+++ modules/chart/index.js
@@ -414,6 +414,7 @@ var MChart = (new function () {
             //Format cells
             _.keys(P).forEach(function (k) {
                 if (k != 'name') {
+                    P[k] = P[k].toString();
                     if (P[k].indexOf(',') != -1 || P[k].indexOf('%') != -1) {
                         P[k] = P[k].replace(',', '');
                         P[k] = P[k].replace('%', '')
