f989cd755dea9ef5c8d99ea109611de170cf0e9e
anssar <rumyancevandr@yandex.ru>
2017-09-08 15:00:25 +0500

    fixes

--- modules/document/index.js
+++ modules/document/index.js
@@ -1,130 +1,138 @@
-var MDocument = (new function () {
+var MDocument = (new function() {
 
-    var self = new Module("document");
+  var self = new Module("document");
 
 
-    self.IsAvailable = function () {
-        return PermChecker.CheckPrivelege("IsDocumentCreator");
+  self.IsAvailable = function() {
+    return PermChecker.CheckPrivelege("IsDocumentCreator");
+  }
+
+
+  self.BeforeHide = function() {
+    self.UnSubscribe()
+  }
+
+  self.BeforeShow = function() {
+    self.Subscribe();
+    self.Show();
+  }
+
+  self.ModelIsLoaded = function() {
+
+  }
+
+  self.ModelIsSaved = function() {
+    if (self.Mode() == "Folders") {
+      self.LoadFolders();
     }
+  }
 
+  self.ModelIsCreated = function() {
 
-    self.BeforeHide = function () {
-        self.UnSubscribe()
+  }
+
+  self.ModelIsDeleted = function() {
+    if (self.Mode() == "Folders") {
+      self.LoadFolders();
     }
+  }
 
-    self.BeforeShow = function () {
-        self.Subscribe();
-        self.Show();
-    }
 
-    self.ModelIsLoaded = function () {
+  self.FolderStructure = ko.observable(null);
 
-    }
-
-    self.ModelIsSaved = function () {
-        if (self.Mode() == "Folders") {
-            self.LoadFolders();
+  self.LoadFolders = function(done) {
+    self.rGet("folders", {}, function(List) {
+      var Tree = {};
+      var FirstLevel = _.filter(List, {
+        CodeParentDocFolder: ""
+      });
+      FirstLevel.forEach(function(FL) {
+        Tree[FL.CodeDocFolder] = {
+          text: "<i class='ace-icon fa " + FL.Icon + " orange'></i> " + FL.NameDocFolder + "." + FL.CodeDocFolder,
+          model: "docfolder",
+          code: FL.CodeDocFolder,
+          type: 'folder',
+          'icon-class': 'red',
+          additionalParameters: {
+            children: {}
+          }
+        };
+      })
+      List.forEach(function(F) {
+        if (F.CodeParentDocFolder != "") {
+          Tree[F.CodeParentDocFolder]['additionalParameters'].children[F.CodeDocFolder] = {
+            text: "<i class='ace-icon fa " + F.Icon + " green'></i> " + F.NameDocFolder + "." + F.CodeDocFolder,
+            type: 'item',
+            'icon-class': 'green',
+            model: "docfolder",
+            code: F.CodeDocFolder,
+            additionalParameters: {
+              children: {}
+            }
+          };
         }
-    }
+      })
+      self.FolderStructure(Tree);
+      return done && done();
+    })
+  }
 
-    self.ModelIsCreated = function () {
+  self.SaveChanges = function() {
+    self.IsLoading(true);
+    self.IsLoading(false);
+  }
 
-    }
-
-    self.ModelIsDeleted = function () {
-        if (self.Mode() == "Folders") {
-            self.LoadFolders();
-        }
-    }
-
-
-    self.FolderStructure = ko.observable(null);
-
-    self.LoadFolders = function (done) {
-        self.rGet("folders", {}, function (List) {
-            var Tree = {};
-            var FirstLevel = _.filter(List, {
-                CodeParentDocFolder: ""
-            });
-            FirstLevel.forEach(function (FL) {
-                Tree[FL.CodeDocFolder] = {
-                    text: "<i class='ace-icon fa " + FL.Icon + " orange'></i> " + FL.NameDocFolder + "." + FL.CodeDocFolder,
-                    model: "docfolder",
-                    code: FL.CodeDocFolder,
-                    type: 'folder',
-                    'icon-class': 'red',
-                    additionalParameters: {
-                        children: {}
-                    }
-                };
-            })
-            List.forEach(function (F) {
-                if (F.CodeParentDocFolder != "") {
-                    Tree[F.CodeParentDocFolder]['additionalParameters'].children[F.CodeDocFolder] = {
-                        text: "<i class='ace-icon fa " + F.Icon + " green'></i> " + F.NameDocFolder + "." + F.CodeDocFolder,
-                        type: 'item',
-                        'icon-class': 'green',
-                        model: "docfolder",
-                        code: F.CodeDocFolder,
-                        additionalParameters: {
-                            children: {}
-                        }
-                    };
-                }
-            })
-            self.FolderStructure(Tree);
-            return done && done();
+  self.Show = function(done) {
+    if (!self.Mode()) return self.InitSetMode("Docs");
+    switch (self.Mode()) {
+      case "Roles":
+        ModelTableEdit.InitModel("role", {
+          IsExtended: 1,
+          CodeRole: 1
+        });
+        break;
+      case "Root":
+        ModelTableEdit.InitModel("row", {
+          CodeRow: 1
+        }, {
+          CodeParentRow: {
+            $in: [null, ""]
+          }
+        });
+        break;
+      case "Folders":
+        self.LoadFolders(function() {
+          ModelTreeEdit.Init({
+            model: "docfolder",
+            parent_code_field: "CodeParentDocFolder",
+            Tree: self.FolderStructure(),
+            wrapper: function(el) {
+              if (el.CodeParentDocFolder != '') {
+                return "<i class='ace-icon fa " + el.Icon + " green'></i> " + el.NameDocFolder + "." + el.CodeDocFolder
+              } else {
+                return "<i class='ace-icon fa " + el.Icon + " orange'></i> " + el.NameDocFolder + "." + el.CodeDocFolder
+              }
+            }
+          });
         })
+        break;
+      case "Labels":
+        ModelTableEdit.InitModel("label");
+        break;
+      case "DocTypes":
+        ModelTableEdit.InitModel("doctype");
+        break;
+      case "Docs":
+        ModelTableEdit.InitModel("doc", {
+          IndexDoc: 1
+        });
+        break;
     }
-
-    self.SaveChanges = function () {
-        self.IsLoading(true);
-        self.IsLoading(false);
-    }
-
-    self.Show = function (done) {
-        if (!self.Mode()) return self.InitSetMode("Docs");
-        switch (self.Mode()) {
-            case "Roles":
-                ModelTableEdit.InitModel("role", {
-                    IsExtended: 1,
-                    CodeRole: 1
-                });
-                break;
-            case "Root":
-                ModelTableEdit.InitModel("row", {
-                    CodeRow: 1
-                }, {
-                    CodeParentRow: {
-                        $in: [null, ""]
-                    }
-                });
-                break;
-            case "Folders":
-                self.LoadFolders(function () {
-                    ModelTreeEdit.Init({
-                        model: "docfolder",
-                        Tree: self.FolderStructure(),
-                    });
-                })
-                break;
-            case "Labels":
-                ModelTableEdit.InitModel("label");
-                break;
-            case "DocTypes":
-                ModelTableEdit.InitModel("doctype");
-                break;
-            case "Docs":
-                ModelTableEdit.InitModel("doc", {
-                    IndexDoc: 1
-                });
-                break;
-        }
-        return done && done()
-    }
+    return done && done()
+  }
 
 
-    return self;
+  return self;
 })
 
 
