7e34645439eff623fa62f4d0caa47a70a9abc8ab
Koviazin Dmitry <kov-av@yandex.ru>
2017-09-30 09:41:33 +0300

    update server script

--- install/update.sh
+++ install/update.sh
@@ -6,4 +6,6 @@ grunt
 node admin.js compile
 npm i
 node admin.js syncsqlstructure
+wget http://dev.jetcalc.com/custom/translate.json -O /htdocs/jetcalc/static/custom/translate.json
+wget http://dev.jetcalc.com/custom/catalogue.json -O /htdocs/jetcalc/static/custom/catalogue.json
 pm2 restart start.json
