995b0909de9672618a29113c8af881e371fee90d
anssar <rumyancevandr@yandex.ru>
2017-10-20 19:12:58 +0500

    tree

--- modules/transaction/index.js
+++ modules/transaction/index.js
@@ -1,39 +1,82 @@
-var MTransaction = (new function () {
+var MTransaction = (new function() {
 
     var self = new Module("transaction");
 
 
-    self.BeforeHide = function () {
+    self.BeforeHide = function() {
         self.UnSubscribe();
     }
 
-    self.BeforeShow = function () {
+    self.BeforeShow = function() {
         self.Subscribe();
         self.Show();
     }
 
-    self.Update = function(){
+    self.Update = function() {
         self.Show();
     }
 
-    self.ModelIsSaved = function () {
+    self.ModelIsSaved = function() {
         self.Update();
     }
-    self.ModelIsDeleted = function(){
+    self.ModelIsDeleted = function() {
         self.Update();
     }
 
-    self.IsAvailable = function () {
+    self.IsAvailable = function() {
         return PermChecker.CheckPrivelegeAny(["IsTransactionTunner"]);
     }
 
-    self.Init = function (done) {
-        return done && done();
+    self.DocRelationsView = ko.observable('table');
+
+    self.ChangeView = function() {
+        if (self.DocRelationsView() == 'table') {
+            self.DocRelationsView('tree');
+        } else {
+            self.DocRelationsView('table');
+        }
     }
 
-    self.SaveChanges = function () {}
+    self.DocRelations = ko.observableArray([]);
 
-    self.Show = function (done) {
+    self.LoadDocRelations = function(done) {
+        $.getJSON('/api/modules/transaction/docrelations', {}, function(data) {
+            self.DocRelations(data);
+            done && done();
+        })
+    }
+
+    self.GetChilds = function(name) {
+        if (name) {
+            return _.map(_.filter(self.DocRelations(), function(DR) {
+                return DR.CodeDocSourse == name;
+            }), function(DR) {
+                return {
+                    name: DR.CodeDocTarget
+                }
+            })
+        } else {
+            var tmp = [];
+            var DRS = self.DocRelations();
+            for (var i = 0; i < DRS.length; i++) {
+                tmp.push(DRS[i].CodeDocTarget);
+            }
+            tmp = _.uniq(tmp)
+            return _.map(_.uniq(_.map(_.filter(self.DocRelations(), function(DR) {
+                return !_.includes(tmp, DR.CodeDocSourse);
+            }), function(DR) {
+                return DR.CodeDocSourse
+            })), function(CDS){return {name: CDS}})
+        }
+    }
+
+    self.Init = function(done) {
+        self.LoadDocRelations(done);
+    }
+
+    self.SaveChanges = function() {}
+
+    self.Show = function(done) {
         if (!self.Mode()) return self.InitSetMode("BillRelations");
         switch (self.Mode()) {
             case "BillRelations":
