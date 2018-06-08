e120e0f4ef1d2c90e1b7102f1da9988813599458
jetcalc <jetcalc@gmail.com>
2018-06-08 12:30:49 +0500

    удалены кривые комментарии из скрипта обновления

--- install/update.sh
+++ install/update.sh
@@ -6,8 +6,6 @@ grunt
 node admin.js compile
 npm i
 node admin.js syncsqlstructure
---wget http://dev.jetcalc.com/custom/translate.json -O /htdocs/jetcalc/static/custom/translate.json
---wget http://dev.jetcalc.com/custom/catalogue.json -O /htdocs/jetcalc/static/custom/catalogue.json
 cd /htdocs/jetcalc
 cp /htdocs/jetcalc/install/catalogue.json /htdocs/jetcalc/static/custom/catalogue.json
 cp /htdocs/jetcalc/install/translate.json /htdocs/jetcalc/static/custom/translate.json
