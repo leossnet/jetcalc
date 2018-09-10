93a1b0cd3193db20e34295ae6896aac56033c80d
Koviazin Dmitry <kov-av@yandex.ru>
2018-09-10 15:18:31 +0300

    fix first start

--- src/init.js
+++ src/init.js
@@ -7,42 +7,22 @@ moment       = require('moment');
 
 
 var init = {
-	checkInitState: function(done){
-		var collectionsToCheck = [
-			'user',
-			'task',
-			'usertask'
-		];
-		async.map(collectionsToCheck,function(model,callback){
-			mongoose.model(model).count({},callback);
-		},function(err,results){
-			if (err) return done(err);
-			else {
-				var res = !Boolean(_.max(results))
-				return done(null,res);
-			}
-		});
-	},
+
 
 	checkState: function(done){
-		init.checkInitState(function(err,initState){
-			if (initState) {
-				var tasks = [init.initAdmin,init.initTask,init.initUserTask];
-				async.series(tasks,function(err){
-					var LIB = require(__base+"lib/helpers/lib.js");
-					LIB.SyncPriveleges(done);
-				});
-			} else {
-				return done(err);
-			}
-		})
-
+		var tasks = [init.initAdmin,init.initTask,init.initUserTask];
+		async.eachSeries(tasks,function(task,next){
+			task(next);
+		},function(err){
+			var LIB = require(__base+"lib/helpers/lib.js");
+			LIB.SyncPriveleges(done);
+		});
 	},
 
 	initAdmin: function(done){
 		var User = mongoose.model('user');
-
 		User.findOne({CodeUser: 'admin'}).exec(function(err,U){
+			console.log("found user",U);
 			if (!U) {
 				U = User();
 				U.CodeUser = 'admin';
@@ -50,7 +30,8 @@ var init = {
 				U.NameUser = 'admin';
 				U.password = 'admin';
 				U.IsConfirmed = true;
-				U.save(null,done);
+				console.log("creating admin user",U);				
+				U.save('admin',done);
 			} else {
 				return done(null);
 			}
@@ -58,7 +39,6 @@ var init = {
 	},
 	initTask: function(done){
 		var Task = mongoose.model('task');
-		var UserTask = mongoose.model('usertask');
 		Task.findOne({CodeTask:'ADMIN'}).exec(function(err,T){
 			if (!T) {
 				T = Task();
@@ -66,8 +46,8 @@ var init = {
 				T.CodeTask = 'ADMIN';
 				T.NameTask = 'Администратор';
 				T.SNameTask = 'Администратор';
-
-				T.save(null,done);
+				console.log("creating task ",T);				
+				T.save('admin',done);
 			} else {
 				return done(null);
 			}
@@ -83,9 +63,10 @@ var init = {
 				// UT.CodeUserTask = 'admin_ADMIN';
 				UT.CodeUser = 'admin';
 				UT.CodeTask = 'ADMIN';
-
-				UT.save(null,done);
+				console.log("creating user task ",UT);
+				UT.save('admin',done);
 			} else {
+
 				return done(null);
 			}
 		})
--- src/plugins.js
+++ src/plugins.js
@@ -6,6 +6,7 @@ var mongoose    = require('mongoose');
 var init = require(__base+'/src/init.js');
 var config = require(__base+'config.js');
 
+
 var RouteIniter = (new function(){
     var self = this;
 
@@ -35,30 +36,9 @@ var RouteIniter = (new function(){
     return self;
 })
 
-
-
-
-
 init.checkState(function(err){
     if (err) console.log(err);
-/*    var path = __base+"modules";
-    lib.listConfigs (path,function(err,list){  
-        var addRouter = require('express').Router();
-        addRouter.get("/", function(req, res, next) {
-            var path = __base+"modules";
-            lib.listConfigs (path,function(err,list){  
-                return res.json(list);
-            })
-        })
-        list.forEach(function(L){
-            var id = L.config.id;
-            addRouter.use('/'+id, require(path+'/'+id+'/api.js'));
-        })    
-        exportRouter.use("/api/modules", addRouter)
-    })
-*/
 })
-
 
 
 
