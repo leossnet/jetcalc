9c075bcf1b3065d4cea27fd29b7b8743ae049732
leossnet <leossnet@gmail.com>
2019-04-14 20:28:05 +0500

    уточнение файла backup.sh

--- install/backup.sh
+++ install/backup.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 now="$(date +'%Y-%m-%d-%H-%M')"
+tmp=jetcalc
 
 cd $HOME
-if [ ! -d jetcalc ] 
+if [ ! -d $tmp ] 
 then
-    mkdir jetcalc
+    mkdir $tmp
 fi
 
-dirname="$HOME/jetcalc/$now"
-cd jetcalc
-mkdir $now
-cd $now
+dirname="$HOME/$tmp"
+cd $tmp
 
 mongodump -d jetcalc
 export PGPASSWORD="postgres"
 pg_dump -h localhost -p 5432 -U postgres -F c -b -v -f $dirname/sql.backup jetcalc
 
-zip -r "$dirname/dump_$now.zip" $dirname
+cd $HOME
+if [ ! -d backup ]
+then
+    mkdir backup
+fi 
+
+zip -r "backup/dump_$now.zip" $dirname
+rm -R $tmp
