991fc9f1dcf2e8e1ccb8747e5f70626c46f89d62
leossnet <leossnet@gmail.com>
2019-08-05 16:23:14 +0500

    в makedemo настроена процедура обновления sql-базы

--- makedemo.js
+++ makedemo.js
@@ -1,9 +1,9 @@
 /**
  * Преобразование базы данных к демо-версии:
- * 1. Удаление всех учетных записей
- * 2. Добавление учетной записи admin с паролем admin
- * 3. Установление во всех коллекциях значение атрибута UserEdit='admin'
- * 4. Установление в sql-таблицах значение поля CodeUser='admin'
+ * а) Удаление всех учетных записей
+ * б) Добавление учетной записи admin с паролем admin
+ * в) Установление во всех коллекциях значение атрибута UserEdit='admin'
+ * г) Установление в sql-таблицах значение поля CodeUser='admin'
  * 
  * Для запуска необходимо выполнить следующие команды:
  *      cd /htdocs/jetcalc
@@ -13,24 +13,58 @@
 
 var shell = require('shelljs'); // https://www.npmjs.com/package/shelljs
 var Menu = require('terminal-menu'); // https://www.npmjs.com/package/terminal-menu
-//global.__base = __dirname + "/";
+
+var anyDB = require('any-db'); // https://www.npmjs.com/package/any-db
+var Config  = require("./config.js");
+var pg = Config.dbconfig.pgsql;
+var ConnectURL = [
+    pg.adapter,"://",pg.userName,":",pg.password,"@",pg.server,":",pg.options.port,"/",pg.options.database
+].join("");
 
 /**
  * Метки пунктов меню
  */
-var Labels = {
-    header     : "Выбор действия\n",
-    heder_line : "-------------------------\n",
-    makedemo   : "Создание демо-базы",
-    exit       : "Выход",
+var Headers = {
+    header          : "Выбор действия\n",
+    heder_line      : "-------------------------\n",
 };
+var Labels = {
+    setadminsql     : "Замена кодов пользователей в sql",
+    setadmincode    : "Замена кодов пользователей в mongo",
+    setadminuser    : "Удаление пользователей кроме admin",
+    exit            : "Выход",
+};
+
+var bye = "\n------------------------------\nPress Ctrl+C or Enter to exit.\n";
+var dstr = "\n==============================";
 
 /**
  * Задачи
  */ 
 var Tasks = {
     makedemo: function(){
-        console.log("run makedemo...");
+        console.log(Labels.makedemo);
+    },
+    setadminsql: function(done){
+        console.log(Labels.setadminsql+dstr);
+        var queryes = [
+            "alter table cells disable trigger trigger_on_cells;"
+            ,"update cells set \"CodeUser\" = 'admin' where \"CodeUser\" != 'admin'"
+            ,"alter table cells enable trigger trigger_on_cells;"
+            ,"update cells_h set \"CodeUser\" = 'admin' where \"CodeUser\" != 'admin'"
+            ,"alter table valuta_rates disable trigger trigger_on_valuta_rates;"
+            ,"update valuta_rates set \"CodeUser\" = 'admin' where \"CodeUser\" != 'admin'"
+            ,"alter table valuta_rates enable trigger trigger_on_valuta_rates;"
+            ,"update valuta_rates_h set \"CodeUser\" = 'admin' where \"CodeUser\" != 'admin'"
+        ];
+        var pool = anyDB.createPool(ConnectURL, {min: 4, max: 10});
+        runQueryes(pool, queryes);
+    },
+    setadminuser: function(){
+        console.log(Labels.setadminuser);
+    },
+    setadmincode: function(){
+        console.log(Labels.setadmincode);
     }
 };
 
@@ -39,21 +73,28 @@ var Tasks = {
  * После завершения выбранной операции нужно нажать Ctrl+C
  */
 function start () {
-    var menu = Menu({ width: 29, x: 4, y: 2 });
+    var menu = Menu({ width: 36, x: 4, y: 2 });
     menu.reset();
-    menu.write(Labels.header);
-    menu.write(Labels.heder_line);
-    menu.add(Labels.makedemo);
-    menu.add(Labels.exit);
+    for (var header in Headers) {
+        menu.write(Headers[header]);
+    }
+    for (var label in Labels ) {
+        menu.add(Labels[label]);
+    }
     menu.on('select', function (label) {
         menu.close();
         shell.exec('clear');
         switch (label){
-            case Labels.makedemo:
-                Tasks.makedemo();
+            case Labels.setadminsql:
+                Tasks.setadminsql();
+            break;
+            case Labels.setadminuser:
+                Tasks.setadminuser();
+            break;
+            case Labels.setadmincode:
+                Tasks.setadmincode();
             break;
         }
-        console.log("Press Ctrl+C or Enter to exit.");
     });
     process.stdin.pipe(menu.createStream()).pipe(process.stdout);
     process.stdin.setRawMode(true);
@@ -68,10 +109,37 @@ function start () {
  */
 var StartTask = "";
 process.argv.forEach(function (val, index, array) {
-  	if (Tasks[val]) StartTask = val;
+    if (Tasks[val]) StartTask = val;
 });
 if (StartTask && Tasks[StartTask]){
-	Tasks[StartTask]();
+    Tasks[StartTask]();
 } else {
     start();
+}
+
+/**
+ * Последовательное выполнение sql-запросов из массива queryes 
+ * с завершающим закрытием пула соединений с базой данных
+ * @param {*} pool 
+ * @param {*} queryes 
+ */
+function runQueryes(pool, queryes){
+    var sql = queryes.shift();
+    pool.query(sql, function(error, result){
+        if (error){
+            console.log("Error: "+error.message+"\npool close."+bye);
+            pool.close();
+        }
+        else {
+            console.log(sql);
+            if (result.command == "UPDATE") console.log("Обновлено "+result.rowCount+" строк.");
+            if ( queryes.length == 0) {
+                console.log("pool close."+bye);
+                pool.close();
+            }
+            else {
+                runQueryes(pool, queryes);
+            }
+        }
+    });
 }
